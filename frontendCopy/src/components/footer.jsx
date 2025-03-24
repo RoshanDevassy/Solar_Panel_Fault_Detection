11931d8353c82c685be1382e656e66e3b2f57ce6
Roshan-Devassy <roshandevassy101112@gmail.com>
2025-03-24 19:48:18 +0530

    folder name changed

--- frontendCopy/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(params) {
-  return (
-    <>
-      <footer className=" py-5">
-        <p className=" justify-self-center">&copy; 2025 My Website</p>
-      </footer>
-    </>
-  );
-}
